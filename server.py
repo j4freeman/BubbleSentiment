154d8f832cbdc81298d6ea2be4f55363d20a0d43
j4freeman <j4freeman@gmail.com>
2018-03-23 14:01:31 -0500

    typo

--- server.py
+++ server.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 # app.run(environ.get('PORT'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
