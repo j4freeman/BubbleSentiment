749197cc6b67acfad90a0f156fd75be17d657970
j4freeman <j4freeman@gmail.com>
2018-03-23 14:00:04 -0500

    fixed flask

--- server.py
+++ server.py
@@ -2,4 +2,8 @@ from os import environ
 from flask import Flask
 
 app = Flask(__name__)
-app.run(environ.get('PORT'))
+# app.run(environ.get('PORT'))
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
